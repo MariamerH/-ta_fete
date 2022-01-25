43a2f0cf88636d633370af95609b250717bb57a3
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-01-25 15:14:03 -0500

    modelos y validaciones

---
+++ app/models/place.rb
@@ -0,0 +1,9 @@
+class Place < ApplicationRecord
+  belongs_to :user
+  has_many :bookings
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price_per_day, presence: true
+  validates :capacity, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :places, dependent: :destroy
+  has_many :bookings
+  validates :first_name, presence: true
+  validates :second_name, presence: true
+  validates :first_name, uniqueness: { scope: :last_name }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
