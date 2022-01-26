c8b81b6a0922791bcc50e0d73c2817390d81bb18
JhonFiUNFV <asrodeno18@gmail.com>
2022-01-26 16:38:50 -0500

    avancepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def about
+    @user = current_user
+    @requested_bookings = Booking.where(user:current_user)
+    @bookings_places = Booking.where(place:current_user.places)
+    @places = Place.all
+  end
 end
