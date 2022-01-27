5f93cdd8eafe5d318f78f741af2614924f4e952c
mel <mel.valle.perez2003@gmail.com>
2022-01-27 17:21:36 -0500

    avance del show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'creando place'
 
-Place.create( name: 'Local 1', address: 'lima', description: 'descripcion', price_per_day: 4, capacity: 12, user_id: 1)
+Place.create( name: 'El Gran Salon', address: 'Lima', description: 'descripcion', price_per_day: 4, capacity: 12, user_id: 1)
+Place.create( name: 'La Montaña', address: 'Trujillo', description: 'descripcion', price_per_day: 4, capacity: 12, user_id: 1)
+Place.create( name: 'La Alberca', address: 'Arequipa', description: 'descripcion', price_per_day: 4, capacity: 12, user_id: 1)
+Place.create( name: 'La Alberca', address: 'Arequipa', description: 'descripcion', price_per_day: 4, capacity: 12, user_id: 1)
+
 
 puts 'place creado'
