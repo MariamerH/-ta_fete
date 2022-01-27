41178c017b9b08f832d071ea4b744a3634fe380f
JhonFiUNFV <asrodeno18@gmail.com>
2022-01-27 01:27:51 -0500

    as

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   resources :places do
     resources :bookings, only: [:new, :create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :bookings, only: :destroy do
+    member do
+      patch 'accept'
+      patch 'reject'
+    end
+  end
+  get '/', to: 'pages#home'
+  get '/about', to: 'pages#about'
 end
